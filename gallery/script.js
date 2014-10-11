6b30bd0e2bfb510c7c50c6287a69b1e696130592
Karyn Armour <karynarmr33@gmail.com>
2014-10-11 15:21:12 -0500

    finished toggle button exercise

--- gallery/script.js
+++ gallery/script.js
@@ -1,1 +1,12 @@
 // Add your code here
+
+$(".filter-button").on("click", function(){
+	console.log(this.id);
+	var className = "." + this.id; 
+console.log(className);
+$(className).toggle();
+//.toggleClass("active")
+
+
+});
+
